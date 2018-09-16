a27c0bb81a15a94f4a1d3d1d3d278f6bfbd19185
Rob Parker <into.the.parker@gmail.com>
2018-09-16 13:12:31 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,12 +3,17 @@
 
 def run_guessing_game
   puts 'Guess a number between 1 and 6.'
+  randomRoll = rand(6)
   input = gets.chomp
   if input == 'exit'
     puts 'Goodbye!'
     return
+  elsif input == randomRoll
+    puts 'You guessed the correct number!'
+  else 
+    puts 'The computer guessed #{randomRoll}'
   end
-  # randomRoll = rand(6)
+  
   
   
   
